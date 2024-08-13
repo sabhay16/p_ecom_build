9c4f5b6967cab81b0c13ab102de3fc0c4bc0e782
sabhay16 <44006363+sabhay16@users.noreply.github.com>
2024-08-12 20:58:46 -0700

    Remove secrets
    Remove secrets

--- petnet/petnet/settingsprod.py
+++ petnet/petnet/settingsprod.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%x5hx-u&ucr3+$!i1%=tw89an79q_^cu!yx)7465ryk56pdoun'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
